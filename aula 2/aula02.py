5ece787ef7da6e965a0720cebec9b81a440a45bd
Brunofelipevs <bvk542006@gmail.com>
2024-12-03 11:32:38 -0300

    777

---
+++ aula 2/aula02.py
@@ -0,0 +1,11 @@
+print('olá mundo')
+
+nome_usuario = 'Bruno' # tipo ->  = str = string
+idade_usuario =18 # tipo -> int = integer
+altura_usuario = 1.78 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool boolean
+
+print('sejam bem-vindo', nome_usuario)
+print('sua idade é', idade_usuario'anos')
+print('vc tem', altura_usuario, "metros.")
+print('voce é maior de idade?', maior_idade)
